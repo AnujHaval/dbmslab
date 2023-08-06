008ce67d217e27968c1f5328f5284444117c06b3
Anuj <anuj.haval@gmail.com>
2023-08-06 19:19:40 +0530

    Initial commit

---
+++ mynitcbase/main.cpp
@@ -0,0 +1,46 @@
+#include "Buffer/StaticBuffer.h"
+#include "Cache/OpenRelTable.h"
+#include "Disk_Class/Disk.h"
+#include "FrontendInterface/FrontendInterface.h"
+#include <iostream>
+int main(int argc, char *argv[]) {
+  Disk disk_run;
+
+  // create objects for the relation catalog and attribute catalog
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
+
+  // load the headers of both the blocks into relCatHeader and attrCatHeader.
+  // (we will implement these functions later)
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  for (int i=0;i<(relCatHeader.numEntries);i++) {
+
+    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
+
+    relCatBuffer.getRecord(relCatRecord, i);
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    for (int j=0;j<(attrCatHeader.numEntries);j++){
+
+      // declare attrCatRecord and load the attribute catalog entry into it
+        Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+        attrCatBuffer.getRecord(attrCatRecord, j);
+
+        if(!strcmp(relCatRecord[RELCAT_REL_NAME_INDEX].sVal,attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal)) {
+          const char *attrType = (attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER )? "NUM" : "STR";
+          printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+        }
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
+//return FrontendInterface::handleFrontend(argc, argv);
+
